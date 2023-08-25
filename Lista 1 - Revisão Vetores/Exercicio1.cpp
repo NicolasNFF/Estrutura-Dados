25ecea152464fa95931d448ba57822aa1d0ccfd4
NicolasNFF <79235449+NicolasNFF@users.noreply.github.com>
2023-08-24 23:31:43 -0300

    Update Exercicio1.cpp

--- Lista 1 - Revisão Vetores/Exercicio1.cpp
+++ Lista 1 - Revisão Vetores/Exercicio1.cpp
@@ -15,24 +15,24 @@ int main() {
     scanf("%d", &tamanho);
     
     
-    // Preencher o vetor A
+    //Solicitando os valores do vetor A
     printf("Digite os elementos do vetor A:\n");
     for (int i = 0; i < tamanho; i++) {
         scanf("%d", &vetorA[i]);
     }
     
-    // Preencher o vetor B
+    // Solicitando os valores do vetor B
     printf("Digite os elementos do vetor B:\n");
     for (int i = 0; i < tamanho; i++) {
         scanf("%d", &vetorB[i]);
     }
     
-    // Calcular a soma e armazenar no vetor Soma
+    // Calculando a soma e armazenando no vetor Soma
     for (int i = 0; i < tamanho; i++) {
         vetorSoma[i] = vetorA[i] + vetorB[i];
     }
     
-    // Exibir o vetor Soma
+    // Exibind o vetor Soma
     printf("Vetor Soma: ");
     for (int i = 0; i < tamanho; i++) {
         printf("%d ", vetorSoma[i]);
